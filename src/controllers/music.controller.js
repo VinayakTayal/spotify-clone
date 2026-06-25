b0b93987a8a88c1526ff4112a8466ccfe7178125
Vinayak Tayal <vinayaktayal2004@gmail.com>
2026-06-25 12:30:27 +0530

    first commit

---
+++ src/controllers/music.controller.js
@@ -0,0 +1,92 @@
+const musicModel = require("../models/music.model");
+const jwt = require("jsonwebtoken");
+const { uploadFile } = require("../services/storage.service");
+const albumModel = require("../models/album.model");
+
+async function createMusic(req, res) {
+  const { title } = req.body;
+  const file = req.file;
+
+  const result = await uploadFile(file.buffer.toString("base64"));
+
+  const music = await musicModel.create({
+    uri: result.url,
+    title,
+    artist: req.user.id,
+  });
+
+  res.status(201).json({
+    message: "Music Created",
+    music: {
+      id: music.id,
+      uri: music.uri,
+      title: music.title,
+      artist: music.artist,
+    },
+  });
+}
+
+async function createAlbum(req, res) {
+  const { title, musics } = req.body;
+  const album = await albumModel.create({
+    title,
+    artist: req.user.id,
+    musics: musics,
+  });
+
+  res.status(201).json({
+    message: "Album Created",
+    album: {
+      id: album.id,
+      title: album.title,
+      artist: album.artist,
+      musics: album.musics,
+    },
+  });
+}
+
+async function getAllMusics(req, res) {
+  const musics = await musicModel
+    .find()
+    .limit(20)
+    .populate("artist", "username email");
+
+  res.status(200).json({
+    message: "Music fetched",
+    musics: musics,
+  });
+}
+
+async function getAllAlbums(req, res) {
+  const albums = await albumModel
+    .find()
+    .select("title artist")
+    .populate("artist", "username email");
+
+  res.status(200).json({
+    message: "Albums fetched",
+    albums: albums,
+  });
+}
+
+async function getAlbumById(req, res) {
+  const albumId = req.params.albumId;
+
+  const album = await albumModel
+    .findById(albumId)
+    .populate("artist", "email, username")
+    .populate("musics");
+
+  res.atatus(200).json({
+    message: "Album Fetched",
+    album: album,
+  });
+}
+
+module.exports = {
+  createMusic,
+  createAlbum,
+  getAllMusics,
+  getAllAlbums,
+  getAlbumById,
+};
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+
+async function authArtist(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(403).json({
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded !== "artist") {
+      return res.status(403).json({
+        message: "You Dont have Access",
+      });
+    }
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+}
+
+async function authUser(req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    res.status(401).json({ message: "Unauthprized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.role !== "user") {
+      return res.status(403).json({ message: "You dont have access" });
+    }
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+}
+
+module.exports = { authArtist, authUser };
